383ec91fa54ea4e53faa0f42bacc8f10f0b0460e
Miquel Massot <miquel.massot@gmail.com>
2016-11-22 18:26:28 +0100

    Added PointCloud callback update

--- Assets/Scripts/RobotNavSts.cs
+++ Assets/Scripts/RobotNavSts.cs
@@ -7,37 +7,40 @@ using UnityEngine;
 
 public class RobotNavSts : ROSBridgeSubscriber
 {
-    public new static string GetMessageTopic()
-    {
-        return "/navigation/nav_sts";
-    }
+  public new static string GetMessageTopic()
+  {
+    //return "/navigation/nav_sts";
+    return "/cola2_navigation/nav_sts";
+  }
 
-    public new static string GetMessageType()
-    {
-        return "auv_msgs/NavSts";
-    }
+  public new static string GetMessageType()
+  {
+    return "auv_msgs/NavSts";
+  }
 
-    public new static ROSBridgeMsg ParseMessage(JSONNode msg)
-    {
-        return new NavStsMsg(msg);
-    }
+  public new static ROSBridgeMsg ParseMessage(JSONNode msg)
+  {
+    return new NavStsMsg(msg);
+  }
 
-    public static void CallBack(ROSBridgeMsg msg)
+  public new static void CallBack(ROSBridgeMsg msg)
+  {
+    Debug.Log("<color=green>INFO:</color> RobotNavSts CallBack!");
+    GameObject robot = GameObject.Find("Robot");
+    if (robot == null)
     {
-        Debug.Log("<color=green>INFO:</color> RobotNavSts CallBack!");
-        GameObject robot = GameObject.Find("Robot");
-        if (robot == null)
-            Debug.Log("<color=red>ERROR:</color> Can't find GameObject Robot");
-        else
-        {
-            NavStsMsg nav_sts = (NavStsMsg) msg;
-            NEDMsg p = nav_sts.GetPosition();
-            Debug.Log("<color=green>INFO:</color> Position is " + p.ToString());
-			RPYMsg o = nav_sts.GetOrientation();
-            Debug.Log("<color=green>INFO:</color> Orientation is " + o.ToString());
-            robot.transform.position = new Vector3(p.GetNorth(), -p.GetDepth(), p.GetEast());
-			robot.transform.rotation = Quaternion.Euler(o.GetRollDegrees(), o.GetYawDegrees(), o.GetPitchDegrees());
-        }
+      Debug.Log("<color=red>ERROR:</color> Can't find GameObject Robot");
     }
+    else
+    {
+      NavStsMsg nav_sts = (NavStsMsg)msg;
+      NEDMsg p = nav_sts.GetPosition();
+      //Debug.Log("<color=green>INFO:</color> Position is " + p.ToString());
+      RPYMsg o = nav_sts.GetOrientation();
+      //Debug.Log("<color=green>INFO:</color> Orientation is " + o.ToString());
+      robot.transform.position = new Vector3(p.GetNorth(), -p.GetDepth(), p.GetEast());
+      robot.transform.rotation = Quaternion.Euler(o.GetRollDegrees(), o.GetYawDegrees(), o.GetPitchDegrees());
+    }
+  }
 }
 
---
+++ Assets/Scripts/RobotPointCloud2.cs
@@ -0,0 +1,36 @@
+﻿using ROSBridgeLib;
+using ROSBridgeLib.sensor_msgs;
+using System.Collections;
+using SimpleJSON;
+using PointCloud;
+using UnityEngine;
+
+public class RobotPointCloud2 : ROSBridgeSubscriber
+{
+    public static Material matVertex;
+
+    public new static string GetMessageTopic()
+    {
+        return "/stereo_down/points2";
+    }
+
+    public new static string GetMessageType()
+    {
+        return "sensor_msgs/PointCloud2";
+    }
+
+    public new static ROSBridgeMsg ParseMessage(JSONNode msg)
+    {
+        return new PointCloud2Msg(msg);
+    }
+
+    public new static void CallBack(ROSBridgeMsg msg)
+    {
+        Debug.Log("<color=green>INFO:</color> RobotPointCloud2 CallBack!");
+        PointCloud2Msg cloud_msg = (PointCloud2Msg)msg;
+        GameObject robot = GameObject.Find("Robot");
+        ArseaViewer viewer = (ArseaViewer)robot.GetComponent(typeof(ArseaViewer));
+        viewer.PushCloud(cloud_msg);
+    }
+}
+

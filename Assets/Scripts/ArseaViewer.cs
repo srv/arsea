5ccaff9b34694f85c3cd7a697edff4e07ff52544
Miquel Massot <miquel.massot@gmail.com>
2016-11-22 18:25:29 +0100

    Convert PointClouds to Prefabs

--- Assets/Scripts/ArseaViewer.cs
+++ Assets/Scripts/ArseaViewer.cs
@@ -1,30 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 using ROSBridgeLib;
+using ROSBridgeLib.sensor_msgs;
+using PointCloud;
 using System.Reflection;
 using System;
 
 
 public class ArseaViewer : MonoBehaviour {
-	public string ROS_IP = "192.168.1.173";
-	public int port = 9090;
-	private ROSBridgeWebSocketConnection ros = null;
+    public string ROS_IP = "192.168.1.173";
+    public int port = 9090;
+    public Material matVertex;
+    private ROSBridgeWebSocketConnection ros = null;
 
-	// the critical thing here is to define our subscribers, publishers and service response handlers
-	void Start() {
-		// Set ROS
-		ros = new ROSBridgeWebSocketConnection("ws://" + ROS_IP, port); 
-		ros.Connect();
+    // the critical thing here is to define our subscribers, publishers and service response handlers
+    void Start() {
+        // Set ROS
+        Debug.Log("<color=green>INFO:</color> Connecting to " + ROS_IP + "...");
+        ros = new ROSBridgeWebSocketConnection("ws://" + ROS_IP, port); 
+        ros.Connect();
         ros.AddSubscriber(typeof(RobotNavSts));
+        ros.AddSubscriber(typeof(RobotPointCloud2));
     }
 
-	// extremely important to disconnect from ROS. Otherwise packets continue to flow
-	void OnApplicationQuit() {
-		if(ros!=null) ros.Disconnect();
-	}
-	
-	// Update is called once per frame in Unity
-	void Update() {
-		ros.Render();
+    // extremely important to disconnect from ROS. Otherwise packets continue to flow
+    void OnApplicationQuit() {
+        if(ros!=null) ros.Disconnect();
+    }
+  
+    // Update is called once per frame in Unity
+    void Update() {
+        ros.Render();
+    }
+
+    public void PushCloud(PointCloud2Msg cloud_msg)
+    {
+        StartCoroutine("DrawCloud", cloud_msg);
+    }
+
+    private IEnumerator DrawCloud(PointCloud2Msg cloud_msg)
+    {
+        PointCloud2Prefab converter = new PointCloud2Prefab(cloud_msg.GetCloud(), cloud_msg.GetHeader().GetSeq(), matVertex);
+        Debug.Log("After PointCloud2Prefab");
+        GameObject clouds = GameObject.Find("PointCloud");
+        //GameObject robot = GameObject.Find("Robot");
+        GameObject cloud_go = Instantiate(converter.GetPrefab(), this.transform.position, this.transform.rotation) as GameObject;
+        cloud_go.transform.parent = clouds.transform;
+        yield return null;
     }
 }
---
+++ Assets/Scripts/PointCloud2Prefab.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+namespace PointCloud
+{
+    public class PointCloud2Prefab
+    {
+        // File
+        private Material _matVertex;
+
+        // PointCloud
+        private GameObject _pointCloud;
+
+        // have in mind that you have a limit of 65k points
+        // per mesh, so if you want to render more points, 
+        // you need to split them.
+        private int _limitPoints = 65000;
+
+        public PointCloud2Prefab(PointCloud<PointXYZRGB> cloud, int idx, Material matVertex)
+        {
+            Debug.Log("PointCloud2Prefab constructor");
+            _matVertex = matVertex;
+            CreateFolders();
+            Load(cloud, idx);
+        }
+
+        public GameObject GetPrefab()
+        {
+            return _pointCloud;
+        }
+
+        private void CreateFolders()
+        {
+            Debug.Log("PointCloud2Prefab CreateFolders");
+            if (!Directory.Exists(Application.dataPath + "/Temp/"))
+            {
+                Debug.Log("Checking if folder " + Application.dataPath + "/Temp/" + " exists...");
+                UnityEditor.AssetDatabase.CreateFolder("Assets", "Temp");
+            }
+            if (!Directory.Exists(Application.dataPath + "/Temp/Clouds/"))
+            {
+                UnityEditor.AssetDatabase.CreateFolder("Assets/Temp", "Clouds");
+            }
+        }
+
+        private void Load(PointCloud<PointXYZRGB> cloud, int global_idx)
+        {
+            Debug.Log("PointCloud2Prefab Load");
+            Vector3[] points = new Vector3[cloud.Size];
+            Color[] colors = new Color[cloud.Size];
+
+            for (int i = 0; i < cloud.Size; i++)
+            {
+                points[i] = new Vector3(cloud[i].X, -cloud[i].Z, cloud[i].Y);
+                colors[i] = new Color((float)cloud[i].R / 255.0f, (float)cloud[i].G / 255.0f, (float)cloud[i].B / 255.0f);
+
+                //if ( i % 1000 == 0)
+                //{
+                //    yield return null;
+                //}
+            }
+
+            // Instantiate Point Groups
+            int numGroups = Mathf.CeilToInt(cloud.Size * 1.0f / _limitPoints * 1.0f);
+
+            _pointCloud = new GameObject("cloud_" + global_idx.ToString());
+
+            for (int i = 0; i < numGroups - 1; i++)
+            {
+                InstantiateMesh(points, colors, global_idx, i);
+                if (i % 10 == 0)
+                {
+                    Debug.Log(i.ToString() + " out of " + numGroups.ToString() + " PointGroups loaded");
+                }
+                //yield return null;
+            }
+
+            //Store PointCloud
+            Debug.Log("CreatePrefab...");
+            UnityEditor.PrefabUtility.CreatePrefab("Assets/Temp/Clouds/" + "cloud_" + global_idx.ToString() + ".prefab", _pointCloud);
+            Debug.Log("return");
+        }
+
+        private void InstantiateMesh(Vector3[] points, Color[] colors, int global_idx, int local_idx)
+        {
+            Debug.Log("InstantiateMesh");
+            // Create Mesh
+            GameObject pointGroup = new GameObject("cloud_" + global_idx.ToString() + "_" + local_idx);
+            pointGroup.AddComponent<MeshFilter>();
+            pointGroup.AddComponent<MeshRenderer>();
+            pointGroup.GetComponent<Renderer>().material = _matVertex;
+
+            pointGroup.GetComponent<MeshFilter>().mesh = CreateMesh(points, colors, local_idx);
+            pointGroup.transform.parent = _pointCloud.transform;
+
+            // Store Mesh
+            if (!Directory.Exists(Application.dataPath + "/Temp/Clouds/" + "cloud_" + global_idx.ToString()))
+            {
+                UnityEditor.AssetDatabase.CreateFolder("Assets/Temp/Clouds", "cloud_" + global_idx.ToString());
+            }
+            UnityEditor.AssetDatabase.CreateAsset(pointGroup.GetComponent<MeshFilter>().mesh, "Assets/Temp/Clouds/" + "cloud_" + global_idx.ToString() + @"/" + "cloud_" + global_idx.ToString() + "_" + local_idx + ".asset");
+            UnityEditor.AssetDatabase.SaveAssets();
+            UnityEditor.AssetDatabase.Refresh();
+        }
+
+        private Mesh CreateMesh(Vector3[] points, Color[] colors, int id)
+        {
+            Debug.Log("CreateMesh");
+            Mesh mesh = new Mesh();
+
+            int nPoints = _limitPoints;
+            if (_limitPoints * (id + 1) > points.Length)
+                nPoints = points.Length - _limitPoints * id;
+
+            Vector3[] localPoints = new Vector3[nPoints];
+            int[] localIndices = new int[nPoints];
+            Color[] localColors = new Color[nPoints];
+
+            for (int i = 0; i < nPoints; ++i)
+            {
+                localPoints[i] = points[id * _limitPoints + i];
+                localIndices[i] = i;
+                localColors[i] = colors[id * _limitPoints + i];
+            }
+
+            mesh.vertices = localPoints;
+            mesh.colors = localColors;
+            mesh.SetIndices(localIndices, MeshTopology.Points, 0);
+            mesh.uv = new Vector2[nPoints];
+            mesh.normals = new Vector3[nPoints];
+
+            return mesh;
+        }
+    }
+}

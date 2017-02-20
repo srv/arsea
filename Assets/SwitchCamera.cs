be3fa01d4932493f5cc9cb6e31396d133ee1edaa
Miquel Massot <miquel.massot@gmail.com>
2017-02-20 17:56:32 +0100

    script to switch from robot camera to external camera

---
+++ Assets/SwitchCamera.cs
@@ -0,0 +1,45 @@
+﻿/* 
+ * @brief ARSEA Project
+ * @author Francisco Bonin Font
+ * @author System, Robotics and Vision
+ * @author University of the Balearic Islands
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class SwitchCamera : MonoBehaviour {
+    public Camera camera1;
+    public Camera camera2;
+    bool cameraindex = false;
+
+    // Use this for initialization
+    void Start () {
+        //turn on the first camera
+        camera1.gameObject.SetActive(true);
+        Debug.Log("Camera Active: " + camera1.gameObject.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            cameraindex = !cameraindex; 
+
+            if (cameraindex)
+            {
+                camera1.gameObject.SetActive(false);
+                camera2.gameObject.SetActive(true);
+            }
+            else
+            {
+                camera1.gameObject.SetActive(true);
+                camera2.gameObject.SetActive(false);
+                cameraindex = false;
+            }
+        }
+
+    }
+}
